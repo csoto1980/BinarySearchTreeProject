d956fc415899d43d711f3e7cd46b27876ac6d9f4
unknown <christinesoto21@gmail.com>
2020-02-28 15:13:57 -0600

    Initial

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        //Member Variables
+        int root = 100;
+
+        //Constructor
+        public BinaryTree()
+        {
+
+        }
+
+        //Methods
+
+        public void Add()
+        {
+
+
+        }
+
+        public void Search()
+        {
+
+
+        }
+
+    }
+}
